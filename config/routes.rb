2c26e2d665f1c6760ed1e85aad5d10ec114ebdf5
Stanislav <valid.bkk@gmail.com>
2019-07-03 13:35:15 +0800

    change index to show current user docs 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :docs
+
   root 'welcome#index'
 
-  resources :docs
 end
